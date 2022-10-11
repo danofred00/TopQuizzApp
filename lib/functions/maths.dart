3c6bfb19dc27fdfc518cd668b4236cda5e312062
Daniel Leussa <leussadaniel04@gmail.com>
2022-10-11 20:06:09 +0100

    Starting WebServer Project"

--- lib/functions/maths.dart
+++ lib/functions/maths.dart
@@ -1,4 +1,7 @@
-
-double byPercent(var value, double scale){
+/*
+  Many mathematical functions are implemented here here
+  
+*/
+double byPercent(var value, double scale) {
   return (value * scale) / 100;
 }
