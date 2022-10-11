2750be38b97fb8bcecf4bc65ba13113d07552fa6
Daniel Leussa <leussadaniel04@gmail.com>
2022-10-11 19:39:07 +0100

    (null)

---
+++ lib/screens/ui/search/search_screen.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+
+class UiSearchPage extends StatelessWidget {
+  const UiSearchPage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      child: Center(
+        child: Text("Search Page"),
+      ),
+    );
+  }
+}
---
+++ lib/screens/ui/settings/settings_screen.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+
+class UiSettingsPage extends StatelessWidget {
+  const UiSettingsPage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      child: Center(
+        child: Text("Settings Page"),
+      ),
+    );
+  }
+}
